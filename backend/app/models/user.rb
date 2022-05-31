e3892dd52d24dadf5399f0f5800933c6070acb2e
StanaAndrei <stadey33@gmail.com>
2022-05-31 13:22:07 +0300

    added email modifier

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+
+    before_save { self.email.downcase! }
 end
